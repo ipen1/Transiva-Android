c4fb7722172f881fc04126bb3b5fa64b832b5cdb
ipen1 <68970625+ipen1@users.noreply.github.com>
2026-07-12 16:06:38 +0800

    Add files via upload

--- app/src/main/java/com/transiva/app/customer/domain/Promo.java
+++ app/src/main/java/com/transiva/app/customer/domain/Promo.java
@@ -1,1 +1,13 @@
+package com.transiva.app.customer.domain;
 
+public final class Promo {
+    public final String title;
+    public final String description;
+    public final String code;
+
+    public Promo(String title, String description, String code) {
+        this.title = title;
+        this.description = description;
+        this.code = code;
+    }
+}
